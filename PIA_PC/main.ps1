2c8b3ee54540e52b69cdd78821e55b26b13d3950
RaulAle119 <raul.gonzalezsvr@uanl.edu.mx>
2024-09-18 15:32:50 -0600

    change
    Se cambio la descripcion del  Get-Help del menu principal

--- PIA_PC/main.ps1
+++ PIA_PC/main.ps1
@@ -3,7 +3,12 @@
 Menu principal, muestra opciones que el usuario podra elegir.#>
 <#
 .DESCRIPTION
-En este menu el usuario podra elegir una de las 5 opciones que corresonden a diferente tareas#>
+En este menu el usuario podra elegir una de las 5 opciones que corresonden a diferente tareas:
+1) Revision de archivos Hashes: Pedira al usuario ingresar el archivo hash que desee analizar
+2) Buscar Archivos Ocultos: Le pedira al usuario el nombre de la carpeta que desea buscar
+3) Revison de recursos del sistema: Arrojara una tabla de datos sobre el Procesador, Disco, RAM y Red
+4) Revisar Conexiones sospechosas: analizara y arrojara una lista de las conoxiones actuales y sus reportes de seguridad.
+5) Salir: Corta la ejecucion #>
 <#
 .NOTES
 Este menu usa funiones de los modulos creados por nosotros, puede elegir una opcion del 1 al 5#>
@@ -32,7 +37,7 @@ do {
     Write-Host "1) Revision de archivos hashes"
     Write-Host "2) Buscar archivos ocultos"
     Write-Host "3) Revision de uso de recursos del sistema"
-    Write-Host "4) Revisas Conexiones sospechosas"
+    Write-Host "4) Revisar Conexiones sospechosas"
     Write-Host "5) Salir"
 
     $opcion = Read-Host
